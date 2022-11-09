88c6fd7e1611bc560231207f4f2512c26bf19fc7
Tiaan Kemp <tiaan.kemp@ap.omron.net>
2022-11-10 12:42:35 +1300

    Assignment 5 Solution V1

--- mod5_solution/js/script.js
+++ mod5_solution/js/script.js
@@ -112,14 +112,14 @@ function buildAndShowHomeHTML (categories) {
       // Hint: you need to surround the chosen category short name with something before inserting
       // it into the home html snippet.
       var name = chosenCategoryShortName.name;
-      var homeHtmlToInsertIntoMainPage = insertProperty(chosenCategoryShortName.toString(),"randomCategoryShortName",name);
+      var homeHtmlToInsertIntoMainPage = insertProperty(chosenCategoryShortName,"randomCategoryShortName",name);
 
 
       // TODO: STEP 4: Insert the produced HTML in STEP 3 into the main page
       // Use the existing insertHtml function for that purpose. Look through this code for an example
       // of how to do that.
       // ....
-      insertHtml("#specials-tile", chosenCategoryShortName);
+      insertHtml("#specials-tile", categoryHtml);
     },
     false); // False here because we are getting just regular HTML from the server, so no need to process JSON.
 }
